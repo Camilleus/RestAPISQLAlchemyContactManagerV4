dc90b4e4c6b87d8d7ec30593fd17e5e914caaf0a
CamilleusRex <camilleusrex@gmail.com>
2024-02-13 19:07:31 +0100

    added docstrings

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 
+# Dodaj middleware do obsługi żądań CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -17,21 +18,26 @@ app.add_middleware(
 )
 
 
+# Dodaj router zdefiniowany w pliku routes.py
 app.include_router(router)
 
 
+# Udostępnij folder 'static' jako zasób statyczny
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
+# Inicjalizuj bazę danych
 init_db()
 
 
+# Importy funkcji z pliku api.apis i klas z pliku schemas.py
 from api.apis import Contact, create_contact, get_all_contacts, get_contact, update_contact, delete_contact, get_birthdays_within_7_days
 from schemas import ContactCreateUpdate, ContactResponse
 
 
 @app.get("/")
 def read_root():
+    """
+    Endpoint główny, zwraca komunikat powitalny.
+    """
     return {"message": "Hello, world!"}
-
-
