60568d309eddc8bd33c714611191336b6680ff6d
CamilleusRex <camilleusrex@gmail.com>
2024-02-13 19:07:50 +0100

    added docstrings

--- api/routes.py
+++ api/routes.py
@@ -13,9 +13,27 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
+    """
+    Read the root endpoint.
+
+    Args:
+        request (Request): The request object.
+
+    Returns:
+        TemplateResponse: Response with the index.html template.
+    """
     return templates.TemplateResponse("index.html", {"request": request, "message": "Hello, world!"})
 
 
 @router.post("/refresh-token/", response_model=Token)
 async def refresh_token(current_token: str = Depends(refresh_access_token)):
+    """
+    Refresh access token.
+
+    Args:
+        current_token (str): The current access token.
+
+    Returns:
+        Token: Response with the new access token.
+    """
     return current_token
