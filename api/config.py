d9358db93996ce7d2736519106f989fa1640ac38
CamilleusRex <camilleusrex@gmail.com>
2024-02-13 19:07:46 +0100

    added docstrings

--- api/config.py
+++ api/config.py
@@ -5,3 +5,16 @@ import os
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+"""
+Konfiguracja aplikacji.
+
+Zawiera informacje dotyczące klucza sekretnego, algorytmu uwierzytelniania
+i schematu uwierzytelniania OAuth2.
+
+Attributes:
+    SECRET_KEY (str): Klucz sekretny aplikacji.
+    ALGORITHM (str): Algorytm uwierzytelniania.
+    oauth2_scheme (OAuth2PasswordBearer): Schemat uwierzytelniania OAuth2.
+"""
